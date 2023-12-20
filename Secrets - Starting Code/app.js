615bf13ed3b327197b620767abb84aa755eb9b06
KanhaiyaKeshri <kanhaiyakeshri101@gmail.com>
2023-12-20 21:30:49 +0530

    LEVEL 3 and 4 authentication using bcrypt

--- Secrets - Starting Code/app.js
+++ Secrets - Starting Code/app.js
@@ -4,9 +4,11 @@ import express from "express";
 import bodyParser from "body-parser";
 import ejs from "ejs";
 import pg from "pg";
+import bcrypt from "bcrypt";
 
 const app = express();
 const port = 3000;
+const saltRounds = 10;
 
 const db = new pg.Client({
     user : "postgres",
@@ -36,14 +38,17 @@ app.get("/register", (req,res) => {
 app.post("/register", async(req,res) =>{
     const email = req.body.username;
     const password = req.body.password;
-    try{
-        await db.query("INSERT INTO user_detail (email, password) VALUES ($1, pgp_sym_encrypt(($2), ($3)))", 
-        [email, password, process.env.SECRET]);
-    }catch(err)
-    {
-        console.log(err);
-    }
-    res.render("secrets.ejs");
+      bcrypt.hash(password, saltRounds, function(err, hash) {
+         try{
+           db.query("INSERT INTO user_detail (email, password) VALUES ($1, $2)", 
+            [email, hash]);
+        }catch(error)
+        {
+            console.log(error);
+        }
+        res.render("secrets.ejs");
+    });
+   
 });
 let user = []; 
 app.post("/login", async(req,res) =>{
@@ -51,8 +56,8 @@ app.post("/login", async(req,res) =>{
     const email = req.body.username;
     const password = req.body.password;
     try{
-      user = await db.query("SELECT email, pgp_sym_decrypt(password, ($1)) AS password FROM user_detail WHERE email=($2)",
-       [process.env.SECRET, email]);
+      user = await db.query("SELECT email, password FROM user_detail WHERE email=($1)",
+       [email]);
      
     }catch(err)
     {
@@ -61,17 +66,22 @@ app.post("/login", async(req,res) =>{
     }
     
     const pass = user.rows[0];
-    const result = pass.password;
+    const result1 = pass.password;
   
     
-    if(result === password){
-        res.render("secrets.ejs");
-    }
-    else{
-        console.log("User Entered password=".password);
-        console.log("Actual password = ", result);
-        res.redirect("/login");
-    }
+    bcrypt.compare(password, result1, function(err, result) {
+        // result == true
+        if(result === true)
+        {
+            res.render("secrets.ejs");
+        }
+        else{
+            console.log("User Entered password=".password);
+            console.log("Actual password = ", result);
+            res.redirect("/login");
+        }
+    });
+        
 
 });
 app.get("/logout",(req,res) =>{
