b328ecc3c1a31a945f7275dc4a8561d76c29f91d
KanhaiyaKeshri <kanhaiyakeshri101@gmail.com>
2023-12-22 20:02:06 +0530

    Google OAuth Authentication-Level 6

--- Secrets - Starting Code/app.js
+++ Secrets - Starting Code/app.js
@@ -2,20 +2,20 @@
 import 'dotenv/config';
 import express from "express";
 import bodyParser from "body-parser";
-import ejs from "ejs";
 import pg from "pg";
 import bcrypt from "bcrypt";
 import session from "express-session";
 import passport from "passport";
 import LocalStrategy from "passport-local";
 import flash from 'express-flash';
+import { Strategy as GoogleStrategy } from "passport-google-oauth2";
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(express.static("public"));
-//app.set('view engine','ejs');
+
 
 const db = new pg.Client({
     user : "postgres",
@@ -37,10 +37,37 @@ app.use(flash());
 
 db.connect();
 
+
+passport.use(new GoogleStrategy({
+  clientID: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  callbackURL: "http://localhost:3000/auth/google/secrets"
+},
+async (accessToken, refreshToken, profile, done) => {
+  try {
+    // Check if the user already exists in the database
+    const user = await db.query("SELECT * FROM user_detail WHERE googleId = $1", [profile.id]);
+
+    if (user.rows.length > 0) {
+      // If the user exists, return the user
+      return done(null, user.rows[0]);
+    } else {
+      // If the user doesn't exist, create a new user in the database
+      const newUser = await db.query("INSERT INTO user_detail (googleId, email, displayName) VALUES ($1, $2, $3) RETURNING *",
+        [profile.id, profile.email, profile.displayName]);
+      
+      return done(null, newUser.rows[0]);
+    }
+  } catch (err) {
+    return done(err, null);
+  }
+}
+));
+
 // Passport local strategy for authentication
 passport.use(new LocalStrategy(async (email, password, done) => {
     try {
-      const user = await db.query("SELECT email, password FROM user_detail WHERE email=($1)", [email]);
+      const user = await db.query("SELECT * FROM user_detail WHERE email=($1)", [email]);
       const userObj = user.rows[0];
   
       if (!userObj) {
@@ -63,24 +90,36 @@ passport.use(new LocalStrategy(async (email, password, done) => {
     }
   }));
 
-  // Passport serialize and deserialize user
 passport.serializeUser((user, done) => {
-    done(null, user.email);
-  });
-  
-  passport.deserializeUser(async (email, done) => {
-    try {
-      const user = await db.query("SELECT email FROM user_detail WHERE email=($1)", [email]);
+  console.log("Serializing user:", user);
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+      const user = await db.query("SELECT * FROM user_detail WHERE id = $1", [id]);
       done(null, user.rows[0]);
-    } catch (err) {
+  } catch (err) {
       done(err, null);
-    }
-  });
+  }
+});
+
+
 
 
 app.get("/", (req,res)=>{
     res.render("home.ejs");
 });
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ["profile"] }));
+
+  app.get("/auth/google/secrets", 
+  passport.authenticate('google', { failureRedirect: "/login "}),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/secrets");
+  });
 
 app.get("/login",(req,res) => {
     res.render("login.ejs", { message : req.flash('error')});
@@ -93,11 +132,19 @@ app.get("/register", (req,res) => {
 app.post("/register", async (req, res) => {
     const email = req.body.username;
     const password = req.body.password;
-  
+     // Check if email and password are provided for manual registration
+     if (!email || !password) {
+      return res.render("register.ejs", { error: "Email and password are mandatory fields." });
+     }
+     // Check if the user is already registered
+    const existingUser = await db.query("SELECT * FROM user_detail WHERE email = $1", [email]);
+    if (existingUser.rows.length > 0) {
+        return res.render("register.ejs", { error: "User with this email is already registered." });
+    }
     bcrypt.hash(password, parseInt(process.env.SALTROUNDS), async (err, hash) => {
       try {
         await db.query("INSERT INTO user_detail (email, password) VALUES ($1, $2)", [email, hash]);
-        res.render("secrets.ejs");
+        res.render("login.ejs",{message : "Registered Successfully !,Login with your registered Email Id and password"});
       } catch (error) {
         console.log(error);
         res.redirect("/register");
@@ -113,12 +160,21 @@ app.post("/login", passport.authenticate('local', {
   }));
   
 
-app.get("/secrets", (req, res) => {
+app.get("/secrets", async(req, res) => {
   console.log("Request to /secrets received.");
   console.log("Is Authenticated?", req.isAuthenticated());
 
   if (req.isAuthenticated()) {
-    res.render("secrets.ejs");
+    try {
+      // Fetch non-null secrets from the database
+      const nonNullSecrets = await db.query("SELECT secret FROM user_detail WHERE secret IS NOT NULL");
+
+      // Render the secrets page with non-null fetched secrets
+      res.render("secrets.ejs", { userWithSecret: nonNullSecrets.rows });
+  } catch (error) {
+      console.error("Error fetching secrets:", error);
+      res.redirect("/login");
+  }
   } else {
     console.log("User not authenticated. Redirecting to /login");
     res.redirect("/login");
@@ -131,6 +187,34 @@ app.get("/logout", (req, res) => {
     }
     res.redirect("/");
   });
+});
+app.get("/submit",(req,res) => {
+  if (req.isAuthenticated()) {
+    res.render("submit.ejs");
+  } else {
+    console.log("User not authenticated. Redirecting to /login");
+    res.redirect("/login");
+  }
+});
+app.post("/submit", async (req, res) => {
+  // Check if the user is authenticated (example: check if there's a user session)
+  if (!req.session || !req.session.passport || !req.session.passport.user) {
+    console.log("User not authenticated");
+    return res.redirect("/login"); // Redirect to the login page if not authenticated
+  }
+
+  const userId = req.session.passport.user; // Assuming you store user information in the session
+  //console.log(userId);
+  const submittedSecret = req.body.secret;
+
+  try {
+      // Insert the submitted secret into the database 
+      await db.query("UPDATE user_detail SET secret = $1 WHERE id = $2", [submittedSecret, userId]);
+      res.redirect("/secrets"); // Redirect to a page displaying secrets
+  } catch (error) {
+      console.log(error);
+      res.redirect("/submit"); // Redirect to the submission page in case of an error
+  }
 });
 
 
